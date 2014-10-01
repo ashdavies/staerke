d157bc3a4b0c5b84437064384a92599a0076a93f
ashdavies <ash.davies@gmail.com>
2014-10-02 00:05:00 +0200

    Refactor code changes

--- mobile/src/main/java/com/chaos/starke/core/RoutineActivity.java
+++ mobile/src/main/java/com/chaos/starke/core/RoutineActivity.java
@@ -1,6 +1,5 @@
 package com.chaos.starke.core;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
@@ -9,18 +8,22 @@ import android.widget.ListView;
 
 import com.chaos.starke.R;
 import com.chaos.starke.adapters.ActivityAdapter;
+import com.chaos.starke.dialogs.CreateActivityDialog;
 import com.chaos.starke.models.Activity;
 import com.chaos.starke.models.Routine;
+import com.shamanland.fab.FloatingActionButton;
+import com.shamanland.fab.ShowHideOnScroll;
+
+import java.util.List;
 
 public class RoutineActivity extends FragmentActivity implements OnClickListener {
 
-    private RoutineActivity context = this;
-
-    private ActivityAdapter adapter;
-    private ListView list;
-    private View header;
+    private ActivityAdapter activityAdapter;
+    private ListView activityListView;
 
     private Routine routine;
+
+    private FloatingActionButton createActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +31,19 @@ public class RoutineActivity extends FragmentActivity implements OnClickListener
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routine);
 
-        Intent intent = getIntent();
-        routine = Routine.findById(Routine.class, intent.getLongExtra("routine", 0));
+        createActivity = (FloatingActionButton) findViewById(R.id.create);
+        createActivity.setOnClickListener(this);
 
-        list = (ListView) findViewById(R.id.cards);
-        adapter = new ActivityAdapter(context, Activity.find(Activity.class, "routine = ?", new String[]{routine.getId().toString()}));
+        long routineId = getIntent().getLongExtra("routine", 0);
+        routine = Routine.findById(Routine.class, routineId);
 
-        list.setAdapter(adapter);
-        list.setOnItemClickListener(adapter);
+        List<Activity> activities = Activity.find(Activity.class, "routine = ?", new String[]{String.valueOf(routineId)});
+        activityAdapter = new ActivityAdapter(this, activities);
 
-        findViewById(R.id.create).setOnClickListener(this);
+        activityListView = (ListView) findViewById(R.id.activities);
+        activityListView.setAdapter(activityAdapter);
+        activityListView.setOnItemClickListener(activityAdapter);
+        activityListView.setOnTouchListener(new ShowHideOnScroll(createActivity));
 
     }
 
@@ -45,11 +51,11 @@ public class RoutineActivity extends FragmentActivity implements OnClickListener
     public void onClick(View view) {
         switch (view.getId()) {
 
-            /*case R.id.create:
+            case R.id.create:
 
                 CreateActivityDialog dialog = new CreateActivityDialog(routine, null);
                 dialog.show(getSupportFragmentManager(), dialog.getClass().getName());
-                return;        */
+                return;
 
         }
     }
