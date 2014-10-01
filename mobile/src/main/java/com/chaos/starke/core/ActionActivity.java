1a8f86f69467fefbd3764f83cc1a2bb9eb421134
ashdavies <ash.davies@gmail.com>
2014-10-02 00:06:39 +0200

    Renamed action interface

--- mobile/src/main/java/com/chaos/starke/core/ActionActivity.java
+++ mobile/src/main/java/com/chaos/starke/core/ActionActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import com.chaos.starke.R;
 import com.chaos.starke.adapters.items.ActionEntry;
 import com.chaos.starke.adapters.items.ActionHeader;
-import com.chaos.starke.actions.ActionItem;
+import com.chaos.starke.adapters.items.ActionInterface;
 import com.chaos.starke.adapters.ActionAdapter;
 import com.chaos.starke.models.Action;
 import com.orm.query.Select;
@@ -22,7 +22,7 @@ public class ActionActivity extends ListActivity {
     final ActionActivity context = this;
 
     private ActionAdapter adapter;
-    private List<ActionItem> items;
+    private List<ActionInterface> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +32,7 @@ public class ActionActivity extends ListActivity {
 
         SimpleDateFormat format = new SimpleDateFormat("cccc dd LLLL", Locale.UK);
 
-        items = new ArrayList<ActionItem>();
+        items = new ArrayList<ActionInterface>();
         List<Action> actions = Select.from(Action.class).orderBy("date DESC").list();
         long last = 0;
 
