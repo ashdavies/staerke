8bd62a029e92c01a9829fc13b8ffd21de5c995f2
ashdavies <ash.davies@gmail.com>
2014-10-13 11:14:21 +0200

    Add categories method with item click receiver to navigation adapter

--- mobile/src/main/java/com/chaos/starke/adapters/NavigationAdapter.java
+++ mobile/src/main/java/com/chaos/starke/adapters/NavigationAdapter.java
@@ -3,6 +3,7 @@ package com.chaos.starke.adapters;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 
 import com.chaos.starke.R;
@@ -12,7 +13,7 @@ import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class NavigationAdapter extends ArrayAdapter<String> {
+public class NavigationAdapter extends ArrayAdapter<String> implements AdapterView.OnItemClickListener {
 
     public NavigationAdapter(Context context) {
         super(context, R.layout.navigation_item, R.id.title, new ArrayList<String>());
@@ -25,9 +26,19 @@ public class NavigationAdapter extends ArrayAdapter<String> {
 
         Routine.Category category = Routine.Category.valueOf(getItem(position));
         CircleImageView thumbnail = (CircleImageView) convertView.findViewById(R.id.thumbnail);
-        thumbnail.setImageResource(RoutineAdapter.Thumbnail(category));
+        thumbnail.setImageResource(category.getIcon());
 
         return convertView;
 
     }
+
+    public void addCategories(Routine.Category[] categories) {
+        for (Routine.Category category : categories) add(category.name());
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+    }
+
 }
