7e9d10514035553803c867eccb9b9146e392569a
ashdavies <ash.davies@gmail.com>
2014-10-13 11:15:11 +0200

    Make routine enum implement get icon methods

--- mobile/src/main/java/com/chaos/starke/models/Routine.java
+++ mobile/src/main/java/com/chaos/starke/models/Routine.java
@@ -1,5 +1,6 @@
 package com.chaos.starke.models;
 
+import com.chaos.starke.R;
 import com.orm.SugarRecord;
 
 public class Routine extends SugarRecord<Routine> {
@@ -9,7 +10,37 @@ public class Routine extends SugarRecord<Routine> {
 
     public Category category;
 
-    public enum Category {Strength, Health, Endurance, Performance, Women}
+    public enum Category implements CategoryWithIcon {
+        Strength {
+            public int getIcon() {
+                return R.drawable.ic_strength;
+            }
+        },
+        Health {
+            public int getIcon() {
+                return R.drawable.ic_health;
+            }
+        },
+        Endurance {
+            public int getIcon() {
+                return R.drawable.ic_endurance;
+            }
+        },
+        Performance {
+            public int getIcon() {
+                return R.drawable.ic_performance;
+            }
+        },
+        Women {
+            public int getIcon() {
+                return R.drawable.ic_women;
+            }
+        }
+    }
+
+    private interface CategoryWithIcon {
+        public int getIcon();
+    }
 
     public Boolean favourite;
 
