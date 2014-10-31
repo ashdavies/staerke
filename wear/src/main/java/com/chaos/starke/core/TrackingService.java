824b08729882b272afca5504634aef26237b8336
ashdavies <ash.davies@gmail.com>
2014-10-31 12:11:22 +0000

    Implemented initial tracking service

--- wear/src/main/java/com/chaos/starke/core/TrackingService.java
+++ wear/src/main/java/com/chaos/starke/core/TrackingService.java
@@ -1,4 +1,62 @@
 package com.chaos.starke.core;
 
-public class TrackingService {
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+import com.chaos.starke.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.data.FreezableUtils;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.Wearable;
+import com.google.android.gms.wearable.WearableListenerService;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class TrackingService extends WearableListenerService {
+
+    private final static int ONGOING_NOTIFICATION_ID = 18;
+
+    private GoogleApiClient googleApiClient;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .build();
+        googleApiClient.connect();
+    }
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEvents) {
+        final List<DataEvent> events = FreezableUtils.freezeIterable(dataEvents);
+        dataEvents.close();
+
+        if (!googleApiClient.isConnected()) {
+            ConnectionResult connectionResult = googleApiClient.blockingConnect(30, TimeUnit.SECONDS);
+            if (!connectionResult.isSuccess()) {
+                Log.e(getPackageName(), "Service failed to connect to GoogleApiClient.");
+                return;
+            }
+        }
+
+        for (DataEvent event : events) {
+            if (event.getType() == DataEvent.TYPE_CHANGED) {
+                Notification.Builder notificationBuilder = new Notification.Builder(this)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher))
+                        .setOngoing(true)
+                        .extend(new Notification.WearableExtender());
+
+                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                notificationManager.notify(ONGOING_NOTIFICATION_ID, notificationBuilder.build());
+            }
+        }
+    }
+
 }
