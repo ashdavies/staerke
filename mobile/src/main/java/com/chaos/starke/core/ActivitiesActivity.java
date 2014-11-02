bee5c437e0674fed02e330f819773b7f17f01ff6
ashdavies <ash.davies@gmail.com>
2014-11-02 15:43:36 +0000

    Add accelerometer graph to activities activity with listeners for wear communication

--- mobile/src/main/java/com/chaos/starke/core/ActivitiesActivity.java
+++ mobile/src/main/java/com/chaos/starke/core/ActivitiesActivity.java
@@ -5,6 +5,7 @@ import android.app.ListActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.LinearLayout;
 
 import com.chaos.starke.R;
@@ -13,6 +14,10 @@ import com.chaos.starke.adapters.items.ActionEntry;
 import com.chaos.starke.adapters.items.ActionHeader;
 import com.chaos.starke.adapters.items.ActionInterface;
 import com.chaos.starke.models.Action;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Wearable;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GraphViewSeries;
 import com.jjoe64.graphview.GraphViewStyle;
@@ -24,13 +29,14 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.Random;
 
-public class ActivitiesActivity extends ListActivity {
+public class ActivitiesActivity extends ListActivity implements GoogleApiClient.ConnectionCallbacks {
 
     private final ActivitiesActivity context = this;
 
     private final int MAXIMUM_HORIZONTAL_AMOUNT = 100;
+
+    private GoogleApiClient googleApiClient;
 
     private GraphViewSeries accelerationDataSeries;
     private GraphView accelerationGraph;
@@ -46,8 +52,31 @@ public class ActivitiesActivity extends ListActivity {
         setContentView(R.layout.activity_activities);
 
         //setupActionBar(getActionBar());
-        setupAccelerationGraph(this, (LinearLayout)findViewById(R.id.acceleration));
+        setupAccelerationGraph(this, (LinearLayout) findViewById(R.id.acceleration));
+        setupActivities();
 
+        setupWearListener(this);
+
+    }
+
+    private void setupActionBar(final ActionBar actionBar) {
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setupAccelerationGraph(final Context context, final LinearLayout container) {
+        accelerationDataSeries = new GraphViewSeries(new GraphView.GraphViewData[]{});
+        accelerationGraph = new LineGraphView(context, context.getString(R.string.activity_activities));
+        accelerationGraph.addSeries(accelerationDataSeries);
+        accelerationGraph.setScrollable(true);
+        accelerationGraph.setShowHorizontalLabels(false);
+        accelerationGraph.getGraphViewStyle().setGridStyle(GraphViewStyle.GridStyle.HORIZONTAL);
+        accelerationGraph.setViewPort(1, MAXIMUM_HORIZONTAL_AMOUNT);
+        accelerationGraph.setScalable(true);
+        container.addView(accelerationGraph);
+    }
+
+    private void setupActivities() {
         SimpleDateFormat format = new SimpleDateFormat("cccc dd LLLL", Locale.GERMANY);
 
         items = new ArrayList<ActionInterface>();
@@ -64,26 +93,12 @@ public class ActivitiesActivity extends ListActivity {
 
         adapter = new ActionAdapter(context, items);
         setListAdapter(adapter);
-
-        testHandler.post(testRunnable);
-
     }
 
-    private void setupActionBar(ActionBar actionBar) {
-        actionBar.setHomeButtonEnabled(true);
-        actionBar.setDisplayHomeAsUpEnabled(true);
-    }
-
-    private void setupAccelerationGraph(Context context, LinearLayout container) {
-        accelerationDataSeries = new GraphViewSeries(new GraphView.GraphViewData[]{});
-        accelerationGraph = new LineGraphView(context, context.getString(R.string.activity_activities));
-        accelerationGraph.addSeries(accelerationDataSeries);
-        accelerationGraph.setScrollable(true);
-        accelerationGraph.setShowHorizontalLabels(false);
-        accelerationGraph.getGraphViewStyle().setGridStyle(GraphViewStyle.GridStyle.HORIZONTAL);
-        accelerationGraph.setViewPort(1, MAXIMUM_HORIZONTAL_AMOUNT);
-        accelerationGraph.setScalable(true);
-        container.addView(accelerationGraph);
+    private void setupWearListener(final ActivitiesActivity context) {
+        googleApiClient = new GoogleApiClient.Builder(context).addApi(Wearable.API).build();
+        googleApiClient.registerConnectionCallbacks(context);
+        googleApiClient.connect();
     }
 
     public void addAccelerationData(double value) {
@@ -92,15 +107,28 @@ public class ActivitiesActivity extends ListActivity {
         currentIndex++;
     }
 
-    Handler testHandler = new Handler();
-    Runnable testRunnable = new Runnable() {
+    @Override
+    public void onConnected(Bundle bundle) {
+        Wearable.MessageApi.addListener(googleApiClient, new MessageApi.MessageListener() {
+            @Override
+            public void onMessageReceived(MessageEvent messageEvent) {
+                Log.i(getPackageName(), "Received value: " + messageEvent.getPath());
+                final Double value = Double.valueOf(messageEvent.getPath());
+                if (value > 0) {
+                    new Handler(getMainLooper()).post(new Runnable() {
+                        @Override
+                        public void run() {
+                            addAccelerationData(value);
+                        }
+                    });
+                }
+            }
+        });
 
-        @Override
-        public void run() {
-            double data = new Random().nextInt(accelerationGraph.getHeight());
-            addAccelerationData(data);
-            testHandler.postDelayed(this, 100);
-        }
-    };
+    }
 
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
 }
