d00004df81be0c4fd13494f98116ceaf76795cb4
ashdavies <ash.davies@gmail.com>
2015-03-29 16:40:04 +0200

    Drop wearable modules

--- shared/src/main/java/com/chaos/starke/shared/SensorEntry.java
+++
@@ -1,7 +0,0 @@
-package com.chaos.starke.shared;
-
-public class SensorEntry {
-    public final static String ACCURACY = "accuracy";
-    public final static String TIMESTAMP = "timestamp";
-    public final static String VALUES = "values";
-}
