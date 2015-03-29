1d422c4f8d122a2ccc7d900b84bd2981a430bf63
ashdavies <ash.davies@gmail.com>
2015-03-29 18:08:36 +0200

    Spinners not required on create activity dialog

--- mobile/src/main/java/com/chaos/starke/dialogs/CreateActivityDialog.java
+++ mobile/src/main/java/com/chaos/starke/dialogs/CreateActivityDialog.java
@@ -10,7 +10,6 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.NumberPicker;
 import android.widget.Spinner;
 
 import com.chaos.starke.R;
@@ -18,21 +17,10 @@ import com.chaos.starke.models.Activity;
 import com.chaos.starke.models.Routine;
 
 public class CreateActivityDialog extends DialogFragment {
-
     private Routine routine;
     private Activity activity;
 
     private Spinner name;
-    private NumberPicker weight;
-    private NumberPicker repetitions;
-    private NumberPicker sets;
-
-    private String[] weightValues;
-    private String[] repetitionValues;
-    private String[] setValues;
-
-    public CreateActivityDialog() {
-    }
 
     public CreateActivityDialog(final Routine routine, final Activity activity) {
         this.routine = routine;
@@ -52,55 +40,12 @@ public class CreateActivityDialog extends DialogFragment {
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, activities);
         this.name.setAdapter(adapter);
 
-        this.weight = (NumberPicker) view.findViewById(R.id.weight);
-        this.weightValues = new String[45];
-        for (int i = 0; i < this.weightValues.length; i++) this.weightValues[i] = Integer.toString(i * 5);
-        this.weight.setMinValue(0);
-        this.weight.setMaxValue(this.weightValues.length - 1);
-        this.weight.setWrapSelectorWheel(false);
-        this.weight.setDisplayedValues(this.weightValues);
-
-        this.repetitions = (NumberPicker) view.findViewById(R.id.repetitions);
-        this.repetitionValues = new String[30];
-        for (int i = 0; i < this.repetitionValues.length; i++) this.repetitionValues[i] = Integer.toString(i);
-        this.repetitions.setMinValue(0);
-        this.repetitions.setMaxValue(this.repetitionValues.length - 1);
-        this.repetitions.setWrapSelectorWheel(false);
-        this.repetitions.setDisplayedValues(this.repetitionValues);
-
-        this.sets = (NumberPicker) view.findViewById(R.id.sets);
-        this.setValues = new String[15];
-        for (int i = 0; i < this.setValues.length; i++) this.setValues[i] = Integer.toString(i);
-        this.sets.setMinValue(0);
-        this.sets.setMaxValue(this.setValues.length - 1);
-        this.sets.setWrapSelectorWheel(false);
-        this.sets.setDisplayedValues(this.setValues);
-
         if (this.activity != null) {
             for (int i = 0; i < activities.length; i++)
                 if (activities[i].equals(this.activity.name)) {
                     this.name.setSelection(i, true);
                     break;
                 }
-
-            for (int i = 0; i < this.weightValues.length; i++)
-                if (this.weightValues[i].equals(String.valueOf(this.activity.weight))) {
-                    this.weight.setValue(i);
-                    break;
-                }
-
-            for (int i = 0; i < this.repetitionValues.length; i++)
-                if (this.repetitionValues[i].equals(String.valueOf(this.activity.repetitions))) {
-                    this.repetitions.setValue(i);
-                    break;
-                }
-
-            for (int i = 0; i < this.setValues.length; i++)
-                if (this.setValues[i].equals(String.valueOf(this.activity.sets))) {
-                    this.sets.setValue(i);
-                    break;
-                }
-
         }
 
         builder.setView(view)
@@ -113,9 +58,6 @@ public class CreateActivityDialog extends DialogFragment {
                         }
                         activity.routine = routine;
                         activity.name = name.getSelectedItem().toString();
-                        activity.weight = Integer.valueOf(weightValues[weight.getValue()]);
-                        activity.repetitions = Integer.valueOf(repetitionValues[repetitions.getValue()]);
-                        activity.sets = Integer.valueOf(setValues[sets.getValue()]);
                         activity.save();
 
                         BackupManager backupManager = new BackupManager(getActivity());
