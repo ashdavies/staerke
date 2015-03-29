f8643f5aabf7b5cea02a04999824e9bd1c4f863f
ashdavies <ash.davies@gmail.com>
2015-03-29 18:20:13 +0200

    Update routine activity with setTitle

--- mobile/src/main/java/com/chaos/starke/core/RoutineActivity.java
+++ mobile/src/main/java/com/chaos/starke/core/RoutineActivity.java
@@ -2,11 +2,6 @@ package com.chaos.starke.core;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ListView;
@@ -21,63 +16,47 @@ import com.shamanland.fab.ShowHideOnScroll;
 
 import java.util.List;
 
-public class RoutineActivity extends ActionBarActivity {
-    private static final int TRACKING_NOTIFICATION_ID = 16;
+import butterknife.InjectView;
 
-    private ActivityAdapter activityAdapter;
-    private ListView activityListView;
+public class RoutineActivity extends BaseActivity implements OnClickListener {
+
+    @InjectView(R.id.activities)
+    protected ListView activityListView;
+
+    @InjectView(R.id.action_button)
+    protected FloatingActionButton actionButton;
 
     private Routine routine;
-
-    private FloatingActionButton actionButton;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.activity_routine);
 
         this.routine = this.getRoutineFromIntent(this.getIntent());
-        this.activityAdapter = new ActivityAdapter(this, this.getActivitiesForRoutine(this.routine));
+        this.setTitle(this.routine.name);
+        
+        this.actionButton.setOnClickListener(this);
 
-        this.setupActionBar(this.getSupportActionBar());
-        this.setupActionButton(this.getSupportFragmentManager());
-        this.setupActivitiesList(this.activityAdapter);
-        //this.setupNotification(this);
+        final ActivityAdapter activityAdapter = new ActivityAdapter(this, this.getActivitiesForRoutine(this.routine));
+        this.setupActivitiesList(activityAdapter);
     }
 
-    private void setupActionBar(final ActionBar actionBar) {
-        actionBar.setHomeButtonEnabled(true);
-        actionBar.setDisplayHomeAsUpEnabled(true);
+    @Override
+    public int onCreateViewId() {
+        return R.layout.activity_routine;
     }
 
-    private void setupActionButton(final FragmentManager fragmentManager) {
-        this.actionButton = (FloatingActionButton) this.findViewById(R.id.action_button);
-        this.actionButton.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(final View view) {
-                CreateActivityDialog dialog = new CreateActivityDialog(RoutineActivity.this.routine, null);
-                dialog.show(fragmentManager, dialog.getClass().getName());
-            }
-        });
+    @Override
+    public void onClick(final View view) {
+        final CreateActivityDialog dialog = new CreateActivityDialog(RoutineActivity.this.routine, null);
+        dialog.show(getSupportFragmentManager(), dialog.getClass().getName());
     }
 
     private void setupActivitiesList(final ActivityAdapter activityAdapter) {
-        this.activityListView = (ListView) findViewById(R.id.activities);
         this.activityListView.setAdapter(activityAdapter);
         this.activityListView.setOnItemClickListener(activityAdapter);
         this.activityListView.setOnItemLongClickListener(activityAdapter);
         this.activityListView.setOnTouchListener(new ShowHideOnScroll(this.actionButton));
-    }
-
-    private void setupNotification(final RoutineActivity routineActivity) {
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(routineActivity)
-                .setSmallIcon(R.drawable.ic_launcher_circle)
-                .setContentTitle(routineActivity.getString(R.string.notification_tracking_workout))
-                .setContentText(routineActivity.getString(R.string.notification_tracking_workout_text));
-
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(routineActivity);
-        notificationManager.notify(TRACKING_NOTIFICATION_ID, notificationBuilder.build());
     }
 
     private Routine getRoutineFromIntent(final Intent intent) {
@@ -86,8 +65,7 @@ public class RoutineActivity extends ActionBarActivity {
     }
 
     private List<Activity> getActivitiesForRoutine(final Routine routine) {
-        String routineId = String.valueOf(routine.getId());
-        return Activity.find(Activity.class, "routine = ?", new String[]{routineId});
+        final String routineId = String.valueOf(routine.getId());
+        return Activity.find(Activity.class, "routine = ?", routineId);
     }
-
 }
