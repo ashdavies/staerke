c4ccd03c497c0bdcf2916ef68b6e4a302f89e4be
ashdavies <ash.davies@gmail.com>
2015-04-15 23:03:18 +0200

    Drop shared folders

--- src/main/java/com/github/pocmo/sensordashboard/shared/ClientPaths.java
+++
@@ -1,6 +0,0 @@
-package com.github.pocmo.sensordashboard.shared;
-
-public class ClientPaths {
-    public static final String START_MEASUREMENT = "/start";
-    public static final String STOP_MEASUREMENT = "/stop";
-}
